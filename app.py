356ddafb680ecb1255ba71b097eeb58004943879
luis-ntonio <luis.gutierrez.g@utec.edu.pe>
2025-03-25 17:26:59 -0500

    initial commit

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def get_token():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=0.0.0.0)
+    app.run(debug=True, host='0.0.0.0', port=4000)
